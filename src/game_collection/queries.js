e64aa5ffd045f0e987982c92a506599dd854c283
Jack Loomis <jackloomis4@gmail.com>
2026-08-14 21:02:56 -0400

    Added genre and rating to POST query

--- src/game_collection/queries.js
+++ src/game_collection/queries.js
@@ -1,6 +1,6 @@
 const getGames = "SELECT * FROM games";
 const getGamesById = "SELECT * FROM games WHERE id = $1";
-const addNewGame = "INSERT INTO games(title, system_id, status_id) VALUES($1, $2, $3) RETURNING *";
+const addNewGame = "INSERT INTO games(title, genre, system_id, status_id, rating) VALUES($1, $2, $3, $4, $5) RETURNING *";
 const updateGame = "UPDATE games SET title = $1, genre = $2, system_id = $3, status_id = $4, rating = $5 WHERE id = $6 RETURNING *";
 
 module.exports = {
